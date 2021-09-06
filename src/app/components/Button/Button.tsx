af2720f24ffd854e6b64b23087c285b27a437dd9
joshua-meyerrr <87307531+joshua-meyerrr@users.noreply.github.com>
2021-09-06 16:12:50 +0200

    Initial commit

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { ReactNode } from 'react';
+import classes from './Button.module.css';
+
+type ButtonProps = {
+  children: ReactNode;
+};
+function Button({ children }: ButtonProps): JSX.Element {
+  return <button className={classes.button}>{children}</button>;
+}
+
+export default Button;
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/api/hello', (_request, response) => {
+  response.send('Hello API!');
+});
+
+app.use('/storybook', express.static('dist/storybook'));
+
+app.use(express.static('dist/app'));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
+});
