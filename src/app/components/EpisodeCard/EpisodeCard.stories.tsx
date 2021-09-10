4bf1d84618d5b0803ecca69c5d563731104ec0e3
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-10 15:01:38 +0200

    🤡 Create Story

---
+++ src/app/components/EpisodeCard/EpisodeCard.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import EpisodeCard from './EpisodeCard';
+
+export default {
+  title: 'Component/EpisodeCard',
+  component: EpisodeCard,
+};
+
+export const Import = (): JSX.Element => (
+  <EpisodeCard
+    type="import"
+    image="https://upload.wikimedia.org/wikipedia/en/4/4b/The_Joe_Rogan_Experience_logo.jpg"
+    show="Philosophize This!"
+    title="Aristotle Pt. 1"
+    time="1:25h"
+    handleOnClick={() => console.log('click')}
+  />
+);
