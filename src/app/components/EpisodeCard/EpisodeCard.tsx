06b8cebd89b8eec297b58108860dc1d6caf194cf
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-13 11:30:56 +0200

    ✨ Add Note Type Rendering

--- src/app/components/EpisodeCard/EpisodeCard.tsx
+++ src/app/components/EpisodeCard/EpisodeCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import TimeIcon from '../assets/TimeIcon';
+import NoteIcon from '../assets/NoteIcon';
+import StarIcon from '../assets/StarIcon';
 import styles from './EpisodeCard.module.css';
 
 type EpisodeCardProps = {
@@ -8,20 +10,28 @@ type EpisodeCardProps = {
   title: string;
   show: string;
   time: string;
-  handleOnClick: () => void;
+  notes?: number;
+  handleOnClick?: () => void;
+  handleButtonClick?: () => void;
   className?: string;
 };
 
 export default function EpisodeCard({
+  type,
   image,
   title,
   show,
   time,
+  notes,
   handleOnClick,
+  handleButtonClick,
   className,
 }: EpisodeCardProps): JSX.Element {
   return (
-    <div className={`${styles.card} ${className}`} onClick={handleOnClick}>
+    <div
+      className={`${styles.card} ${className} ${styles[`type--${type}`]}`}
+      onClick={handleOnClick}
+    >
       <img
         className={styles.card__image}
         src={image}
@@ -32,13 +42,38 @@ export default function EpisodeCard({
           <p className={styles.info__title}>{title}</p>
           <p className={styles.info__show}>{show}</p>
         </div>
-        <div className={styles.info__time}>
-          <TimeIcon width={13} height={13} />
-          <p className={styles.time__text}>{time}</p>
+        <div className={styles.info__icons}>
+          {type === 'note' ? (
+            <>
+              <div className={styles.icons__container}>
+                <div>
+                  <TimeIcon width={13} height={13} />
+                  <p className={styles.icons__text}>{time}</p>
+                </div>
+                <div>
+                  <NoteIcon width={13} height={13} />
+                  <p className={styles.icons__text}>{notes} Notes</p>
+                </div>
+                <div>
+                  <StarIcon width={14} height={14} />
+                </div>
+              </div>
+              <p className={styles.share}>share</p>
+            </>
+          ) : (
+            <div className={styles.icons__timeWrapper}>
+              <TimeIcon width={13} height={13} />
+              <p className={styles.icons__text}>{time}</p>
+            </div>
+          )}
         </div>
       </div>
-      <button>+</button>
-      {/* TODO Change with Button Component */}
+      {type === 'import' && (
+        <button className={styles.addButton} onClick={handleButtonClick}>
+          +
+        </button>
+      )}
+      {/* TODO Change with Button and Icon Component */}
     </div>
   );
 }
