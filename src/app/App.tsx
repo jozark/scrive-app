3f82a24806ae4c9d7c1a53e24f1dd25db0b1b32b
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-14 11:52:33 +0200

    🔥 Remove App Placeholder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.css';
+import Spotify from './pages/Spotify/Spotify';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
@@ -8,47 +9,14 @@ function App(): JSX.Element {
 
   return (
     <BrowserRouter>
-      <div className={styles.App}>
-        <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
-          <p>Hello Vite + React!</p>
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a
-              className={styles['App-link']}
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className={styles['App-link']}
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-          <Switch>
-            <Route path="/about">
-              <main>About</main>
-            </Route>
-            <Route path="/">
-              <main>Home</main>
-            </Route>
-          </Switch>
-        </header>
-      </div>
+      <Switch>
+        <Route path="/about">
+          <Spotify />
+        </Route>
+        <Route path="/">
+          <main>Home</main>
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
