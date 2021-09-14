36513458d33e3e504569858def0f0f9201d751d9
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-14 11:10:55 +0200

    ♻️ Refactor to use Typography

--- src/app/components/EpisodeCard/EpisodeCard.tsx
+++ src/app/components/EpisodeCard/EpisodeCard.tsx
@@ -4,6 +4,7 @@ import NoteIcon from '../assets/NoteIcon';
 import StarIcon from '../assets/StarIcon';
 import Fallback from '../assets/fallbackImage.png';
 import styles from './EpisodeCard.module.css';
+import Typography from '../Typography/Typography';
 
 type EpisodeCardProps = {
   type: 'note' | 'import';
@@ -40,30 +41,34 @@ export default function EpisodeCard({
       />
       <section className={styles.card__info}>
         <div>
-          <p className={styles.info__title}>{title}</p>
-          <p className={styles.info__show}>{show}</p>
+          <Typography type="h3" className={styles.info__title}>
+            {title}
+          </Typography>
+          <Typography type="subHeading" className={styles.info__show}>
+            {show}
+          </Typography>
         </div>
         <div className={styles.info__icons}>
           {type === 'note' ? (
             <>
               <div className={styles.icons__container}>
                 <div>
-                  <TimeIcon width={13} height={13} />
+                  <TimeIcon width={12} height={12} />
                   <p className={styles.icons__text}>{time}</p>
                 </div>
                 <div>
-                  <NoteIcon width={13} height={13} />
+                  <NoteIcon width={12} height={12} />
                   <p className={styles.icons__text}>{notes} Notes</p>
                 </div>
                 <div>
-                  <StarIcon width={14} height={14} />
+                  <StarIcon width={11} height={11} />
                 </div>
               </div>
               <p className={styles.share}>share</p>
             </>
           ) : (
             <div className={styles.icons__timeWrapper}>
-              <TimeIcon width={13} height={13} />
+              <TimeIcon width={12} height={12} />
               <p className={styles.icons__text}>{time}</p>
             </div>
           )}
