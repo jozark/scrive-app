fb01021fbc27ae694bb917c3850c87e8e6a270b5
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-16 10:33:50 +0200

    ✨ conditional rendering based on token existence

--- src/app/pages/Spotify/Spotify.tsx
+++ src/app/pages/Spotify/Spotify.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import EpisodeCard from '../../components/EpisodeCard/EpisodeCard';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Header from '../../components/Header/Header';
 import styles from './Spotify.module.css';
+import SpotifyLoginButton from '../../components/SpotifyLoginButton/SpotifyLoginButton';
 
 const mockData = [
   {
@@ -58,30 +59,49 @@ const mockData = [
 export default function Spotify(): JSX.Element {
   const [searchValue, setSearchValue] = useState('');
 
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    async function getToken(): Promise<void> {
+      const response = await fetch('/api/auth/token');
+      const data = await response.json();
+      setToken(data.token);
+    }
+
+    getToken();
+  }, []);
   return (
     <main className={styles.container}>
-      <Header className={styles.header} type="default">
-        Import
-      </Header>
-      <SearchBar
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-        handleSearch={() => console.log('search in progress')}
-      />
-      <div className={styles.cardWrapper}>
-        {mockData !== null &&
-          mockData.map((data, i) => (
-            <EpisodeCard
-              handleButtonClick={() => console.log('click')}
-              key={i}
-              type="import"
-              image={data.image}
-              title={data.title}
-              show={data.show}
-              time={data.duration}
-            />
-          ))}
-      </div>
+      {!token ? (
+        <SpotifyLoginButton url="/api/auth/login">
+          Connect with Spotify
+        </SpotifyLoginButton>
+      ) : (
+        <>
+          <Header className={styles.header} type="default">
+            Import
+          </Header>
+          <SearchBar
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            handleSearch={() => console.log('search in progress')}
+          />
+          <div className={styles.cardWrapper}>
+            {mockData !== null &&
+              mockData.map((data, i) => (
+                <EpisodeCard
+                  handleButtonClick={() => console.log('click')}
+                  key={i}
+                  type="import"
+                  image={data.image}
+                  title={data.title}
+                  show={data.show}
+                  time={data.duration}
+                />
+              ))}
+          </div>
+        </>
+      )}
     </main>
   );
 }
