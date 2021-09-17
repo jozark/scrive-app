d7206b594f25f5c046dd49b100e2d943ecc2e4df
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-17 16:48:40 +0200

    ✨ Craete /api/player router

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 
 import auth from './lib/routes/auth';
 import episode from './lib/routes/episode';
+import player from './lib/routes/player';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -18,11 +19,17 @@ app.use('/api/auth', auth);
 
 app.use('/api/episodes', episode);
 
+app.use('/api/player', player);
+
 app.get('/api/hello', (_request, response) => {
   response.send('Hello API!');
 });
 
 app.use('/storybook', express.static('dist/storybook'));
+
+app.get('*', (_request, response) => {
+  response.sendFile('index.html', { root: 'dist/app' });
+});
 
 app.use(express.static('dist/app'));
 
