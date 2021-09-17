954da872f826cc52ee2e74f283a3a75e7285a338
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-17 16:50:51 +0200

    ✨ capture device ID and test to play episode

--- src/app/components/Player/Player.tsx
+++ src/app/components/Player/Player.tsx
@@ -6,6 +6,7 @@ export default function TestPlayer(): JSX.Element {
   const [player, setPlayer] = useState<Spotify.Player | null>(null);
   const [is_paused, setPaused] = useState(false);
   const [is_active, setActive] = useState(false);
+  const [deviceId, setDeviceId] = useState('');
 
   // Auth
   const [token, setToken] = useState('');
@@ -45,23 +46,42 @@ export default function TestPlayer(): JSX.Element {
   window.onSpotifyWebPlaybackSDKReady = () => {
     const accessToken = token;
     const player = new Spotify.Player({
-      name: 'Web Playback SDK Quick Start Player',
+      name: 'Start Player',
       getOAuthToken: (cb) => {
         cb(accessToken);
       },
-      volume: 0.5,
+      volume: 0.2,
     });
+
+    //Error Handling
+
+    player.addListener('initialization_error', ({ message }) => {
+      console.log(message);
+    });
+    player.addListener('authentication_error', ({ message }) => {
+      console.log(message);
+    });
+    player.addListener('account_error', ({ message }) => {
+      console.log(message);
+    });
+    player.addListener('playback_error', ({ message }) => {
+      console.log(message);
+    });
+
     setPlayer(player);
 
     player.addListener('ready', ({ device_id }) => {
       console.log('Ready with Device ID', device_id);
+      setDeviceId(device_id);
     });
 
     player.addListener('not_ready', ({ device_id }) => {
       console.log('Device ID has gone offline', device_id);
+      setDeviceId(device_id);
     });
 
     player.connect();
+
     player.addListener('player_state_changed', (state) => {
       if (!state) {
         return;
@@ -75,6 +95,12 @@ export default function TestPlayer(): JSX.Element {
       });
     });
   };
+
+  function handlePlayEpisode(): void {
+    fetch(`/api/player/${deviceId}/spotify:episode:512ojhOuo1ktJprKbVcKyQ`, {
+      method: 'PUT',
+    });
+  }
 
   return (
     <div className={styles.container}>
@@ -122,6 +148,8 @@ export default function TestPlayer(): JSX.Element {
       >
         &gt;&gt;
       </button>
+
+      <button onClick={handlePlayEpisode}>PLAY THIS SONG</button>
     </div>
   );
 }
