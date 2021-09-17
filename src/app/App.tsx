3bfc11a3764b0aa2a7c7a083abf2cc6fa26ea30a
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-17 12:28:02 +0200

    ⚰️ remove old player componentn

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Spotify from './pages/Spotify/Spotify';
+import TestPlayer from './components/TestPlayer/TestPlayer';
 
 function App(): JSX.Element {
   return (
@@ -8,6 +9,9 @@ function App(): JSX.Element {
       <Switch>
         <Route path="/spotify">
           <Spotify />
+        </Route>
+        <Route path="/player">
+          <TestPlayer />
         </Route>
         <Route path="/">
           <h1>Hello</h1>
