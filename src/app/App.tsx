065efc841a4b0a62717810c4922e6cff1370dc8d
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-21 18:12:52 +0200

    ✨ Create Drawer Route to test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Drawer from './components/Drawer/Drawer';
 import Home from './pages/Home/Home';
 import Spotify from './pages/Spotify/Spotify';
 
@@ -9,6 +10,9 @@ function App(): JSX.Element {
       <Switch>
         <Route path="/spotify">
           <Spotify />
+        </Route>
+        <Route path="/Drawer">
+          <Drawer />
         </Route>
         <Route path="/">
           <Home />
