e46bec15ed96eaaa30ccf3455c05c11b49e9800c
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-23 13:12:44 +0200

    ✨ Initialize Player in OverlayPlayer

--- src/app/components/OverlayPlayer/OverlayPlayer.tsx
+++ src/app/components/OverlayPlayer/OverlayPlayer.tsx
@@ -1,25 +1,132 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PauseIcon from '../assets/PauseIcon';
 import PlayIcon from '../assets/PlayIcon';
 import Button from '../Button/Button';
 import Typography from '../Typography/Typography';
 import styles from './OverlayPlayer.module.css';
 
-export default function OverlayPlayer(): JSX.Element {
+const track = {
+  name: '',
+  album: {
+    images: [{ url: '' }],
+  },
+  artists: [{ name: '' }],
+};
+
+type OverlayPlayerProps = {
+  token: string;
+  className: string;
+};
+
+export default function OverlayPlayer({
+  token,
+  className,
+}: OverlayPlayerProps): JSX.Element {
+  const [player, setPlayer] = useState<Spotify.Player | null>(null);
+  const [isPaused, setPaused] = useState(false);
+  const [isActive, setActive] = useState(false);
+  const [currentTrack, setCurrentTrack] = useState(track);
+  const [playbackProgress, setPlaybackProgress] = useState(0);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = 'https://sdk.scdn.co/spotify-player.js';
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    const accessToken = token;
+    const player = new Spotify.Player({
+      name: 'Scrive',
+      getOAuthToken: (cb) => {
+        cb(accessToken);
+      },
+      volume: 0.5,
+    });
+
+    //Error Handling
+
+    player.addListener('initialization_error', ({ message }) => {
+      console.log(message);
+    });
+    player.addListener('authentication_error', ({ message }) => {
+      console.log(message);
+    });
+    player.addListener('account_error', ({ message }) => {
+      console.log(message);
+    });
+    player.addListener('playback_error', ({ message }) => {
+      console.log(message);
+    });
+
+    setPlayer(player);
+
+    player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+      // playEpisode(device_id);
+    });
+
+    player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+
+    player.connect();
+
+    player.addListener('player_state_changed', (state) => {
+      if (!state) {
+        return;
+      }
+
+      const { duration, position, paused, track_window } = state;
+
+      setCurrentTrack(track_window.current_track);
+      setPaused(paused);
+      setPlaybackProgress(position / duration);
+      player.getCurrentState().then((state) => {
+        !state ? setActive(false) : setActive(true);
+      });
+    });
+  };
+
+  // async function playEpisode(device_id: string): Promise<void> {
+  //   await fetch(
+  //     `/api/player/${device_id}/spotify:episode:0eIW62kUIwcldAA3raqzSq`,
+  //     {
+  //       method: 'PUT',
+  //     }
+  //   );
+  // }
+
   return (
-    <div className={styles.bar}>
+    <div className={`${styles.bar} ${className}`}>
       <img
-        src="https://i.scdn.co/image/595e27535dc0a4f2e5a050c6392ebade0f9affc8"
+        src={
+          currentTrack.album.images[currentTrack.album.images.length - 1].url
+        }
         alt=""
         className={styles.cover}
       />
       <div className={styles.text}>
         <Typography type="p" className={styles.text__head}>
-          Aritotle Pt. 2
+          {currentTrack.name}
         </Typography>
-        <p className={styles.text__sub}>Philosophize This</p>
+        <p className={styles.text__sub}>{currentTrack.artists[0].name}</p>
       </div>
-      <Button type={'squareSmall'} className={styles.play}>
-        <PlayIcon width={12} height={12} fill="#fff" />
+      <Button
+        type={'squareSmall'}
+        className={styles.play}
+        onButtonClick={() => player?.togglePlay()}
+      >
+        {isPaused ? (
+          <PlayIcon width={12} height={12} fill="#fff" />
+        ) : (
+          <PauseIcon width={12} height={12} />
+        )}
       </Button>
     </div>
   );
