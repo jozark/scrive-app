221aec88589828b9f05ee771a4c0607b761477ff
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-23 15:55:44 +0200

    ✨ Create Context for DeviceID and PlayState

--- src/app/context/PlayerContext.tsx
+++ src/app/context/PlayerContext.tsx
@@ -1,41 +1,35 @@
-import type { ReactNode } from 'react';
-import React, { createContext, useState, useEffect } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
+import React, { useState, createContext } from 'react';
 
-type ContextProps = {
-  children: ReactNode;
+type PlayerProps = {
+  deviceID: string;
+  playerIsActive: boolean;
+  setDeviceID: Dispatch<SetStateAction<string>>;
+  setPlayerIsActive: Dispatch<SetStateAction<boolean>>;
 };
 
-const PlayerContext = createContext({ player: null });
+type ContextProps = {
+  children: React.ReactNode;
+};
 
-export const AppProvider: JSX.Element = (props: ContextProps): JSX.Element => {
-  const [data, setData] = useState<{ player: Spotify.Player | null }>({
-    player: null,
-  });
+export const PlayerContext = createContext<PlayerProps>({
+  deviceID: '',
+  playerIsActive: false,
+  setDeviceID: () => null,
+  setPlayerIsActive: () => null,
+});
 
-  async function getToken(): Promise<string> {
-    const response = await fetch('/api/auth/token');
-    const data = await response.json();
-    return data.token;
-  }
-  useEffect(() => {
-    const run = async () => {
-      const token = await getToken();
-      setData({
-        player: new Spotify.Player({
-          name: 'Start Player',
-          getOAuthToken: (cb) => {
-            cb(token);
-          },
-          volume: 0.5,
-        }),
-      });
-    };
-    run();
-  }, []);
+export default function PlayerContextProvider(
+  props: ContextProps
+): JSX.Element {
+  const [playerIsActive, setPlayerIsActive] = useState<boolean>(false);
+  const [deviceID, setDeviceID] = useState<string>('');
 
   return (
-    <PlayerContext.Provider value={{ ...data }}>
+    <PlayerContext.Provider
+      value={{ deviceID, playerIsActive, setDeviceID, setPlayerIsActive }}
+    >
       {props.children}
     </PlayerContext.Provider>
   );
-};
+}
