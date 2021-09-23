8af0e45acb8c7b899e853a13d8a6138b6b5a67b9
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-23 17:49:41 +0200

    ✨ Add expanded Card

--- src/app/components/NoteCard/NoteCard.tsx
+++ src/app/components/NoteCard/NoteCard.tsx
@@ -4,42 +4,92 @@ import TimeIcon from '../assets/TimeIcon';
 import Button from '../Button/Button';
 import Typography from '../Typography/Typography';
 import styles from './NoteCard.module.css';
+import TextareaAutosize from 'react-textarea-autosize';
 
 type NoteCardProps = {
-  title: string;
-  content: string;
+  expanded: boolean;
+  titleValue: string;
+  contentValue: string;
   timestampBegin: string;
   handleOnCardClick: () => void;
   handleOnButtonClick: () => void;
+  handleOnTimestampClick: () => void;
+  setTitleValue: (titleValue: string) => void;
+  setContentValue: (contentValue: string) => void;
 };
 
 function NoteCard({
-  title,
-  content,
+  expanded,
+  titleValue,
+  setTitleValue,
+  contentValue,
+  setContentValue,
   timestampBegin,
   handleOnCardClick,
   handleOnButtonClick,
+  handleOnTimestampClick,
 }: NoteCardProps): JSX.Element {
   return (
-    <div className={styles.card} onClick={handleOnCardClick}>
-      <article>
-        <Typography type="h3" className={styles.title}>
-          {title}
-        </Typography>
-        <div className={styles.timestamp}>
-          <TimeIcon width={12} height={12} />
-          <p className={styles.timestamp__text}>{timestampBegin}</p>
+    <>
+      {expanded ? (
+        <div className={styles.expandedCard}>
+          <div className={styles.header}>
+            <div className={styles.header__time}>
+              <TimeIcon width={16} height={16} />
+              <Typography type="p">Timestamp:</Typography>
+              <p
+                className={styles.time__begin}
+                onClick={handleOnTimestampClick}
+              >
+                {timestampBegin}
+              </p>
+            </div>
+            <Button
+              className={styles.addButton}
+              type="squareSmall"
+              onButtonClick={handleOnButtonClick}
+            >
+              <PlayIcon width={12} height={12} fill="#fff" />
+            </Button>
+          </div>
+          <form className={styles.form}>
+            <input
+              className={styles.form__title}
+              type="text"
+              placeholder="Titel..."
+              value={titleValue}
+              onChange={(event) => setTitleValue(event.target.value)}
+            />
+            <TextareaAutosize
+              className={styles.form__content}
+              minRows={10}
+              value={contentValue}
+              onChange={(event) => setContentValue(event.target.value)}
+            />
+          </form>
         </div>
-        <section className={styles.content}>{content}</section>
-      </article>
-      <Button
-        className={styles.addButton}
-        type="squareSmall"
-        onButtonClick={handleOnButtonClick}
-      >
-        <PlayIcon width={12} height={12} fill="#fff" />
-      </Button>
-    </div>
+      ) : (
+        <div className={styles.card} onClick={handleOnCardClick}>
+          <article>
+            <Typography type="h3" className={styles.title}>
+              {titleValue}
+            </Typography>
+            <div className={styles.timestamp}>
+              <TimeIcon width={12} height={12} />
+              <p className={styles.timestamp__text}>{timestampBegin}</p>
+            </div>
+            <section className={styles.content}>{contentValue}</section>
+          </article>
+          <Button
+            className={styles.addButton}
+            type="squareSmall"
+            onButtonClick={handleOnButtonClick}
+          >
+            <PlayIcon width={12} height={12} fill="#fff" />
+          </Button>
+        </div>
+      )}
+    </>
   );
 }
 
