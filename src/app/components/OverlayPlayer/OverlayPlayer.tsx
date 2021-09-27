94e3d3b6a8d40b221b6519ffb59a3f352d7f4ac9
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-27 13:27:38 +0200

    ✨ Create Basic Structure for Detailed PlayerView

--- src/app/components/OverlayPlayer/OverlayPlayer.tsx
+++ src/app/components/OverlayPlayer/OverlayPlayer.tsx
@@ -5,6 +5,10 @@ import Button from '../Button/Button';
 import Typography from '../Typography/Typography';
 import styles from './OverlayPlayer.module.css';
 import { PlayerContext } from '../../context/PlayerContext';
+import Header from '../Header/Header';
+import PlayControls from '../PlayControls/PlayControls';
+import Drawer from '../Drawer/Drawer';
+import { animated, useSpring } from 'react-spring';
 
 const track = {
   name: '',
@@ -16,20 +20,31 @@ const track = {
 
 type OverlayPlayerProps = {
   token: string;
-  className: string;
 };
 
 export default function OverlayPlayer({
   token,
-  className,
 }: OverlayPlayerProps): JSX.Element {
   const [player, setPlayer] = useState<Spotify.Player | null>(null);
   const [isPaused, setPaused] = useState(false);
   const [isActive, setActive] = useState(false);
   const [currentTrack, setCurrentTrack] = useState(track);
   const [playbackProgress, setPlaybackProgress] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const { setDeviceID, playerIsActive } = useContext(PlayerContext);
+  const scaleProps = useSpring({
+    transform: isOpen ? 'scale(0.6)' : 'scale(1)',
+    from: { transform: 'scale(1)' },
+    config: { friction: isOpen ? 18 : 21 },
+  });
+
+  const vanishProps = useSpring({
+    transform: isOpen ? 'scale(0)' : 'scale(1)',
+    from: { transform: 'scale(1)' },
+  });
+
+  const { setDeviceID, playerIsActive, playerIsDetailed, setPlayerIsDetailed } =
+    useContext(PlayerContext);
 
   useEffect(() => {
     const script = document.createElement('script');
@@ -101,32 +116,90 @@ export default function OverlayPlayer({
     return <></>;
   }
 
+  // const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <div className={`${styles.bar} ${className}`}>
-      <img
-        src={
-          currentTrack.album.images[currentTrack.album.images.length - 1].url
-        }
-        alt=""
-        className={styles.cover}
-      />
-      <div className={styles.text}>
-        <Typography type="p" className={styles.text__head}>
-          {currentTrack.name}
-        </Typography>
-        <p className={styles.text__sub}>{currentTrack.artists[0].name}</p>
-      </div>
-      <Button
-        type={'squareSmall'}
-        className={styles.play}
-        onButtonClick={() => player?.togglePlay()}
+    // <div className={`${styles.bar} ${className}`}>
+    //   <img
+    //     src={
+    //       currentTrack.album.images[currentTrack.album.images.length - 1].url
+    //     }
+    //     alt=""
+    //     className={styles.cover}
+    //   />
+    //   <div className={styles.text}>
+    //     <Typography type="p" className={styles.text__head}>
+    //       {currentTrack.name}
+    //     </Typography>
+    //     <p className={styles.text__sub}>{currentTrack.artists[0].name}</p>
+    //   </div>
+    //   <Button
+    //     type={'squareSmall'}
+    //     className={styles.play}
+    //     onButtonClick={() => player?.togglePlay()}
+    //   >
+    //     {isPaused ? (
+    //       <PlayIcon width={12} height={12} fill="#fff" />
+    //     ) : (
+    //       <PauseIcon width={12} height={12} />
+    //     )}
+    //   </Button>
+    // </div>
+    <div className={styles.detailContainer}>
+      <div
+        className={`${styles.mainPlayer} ${
+          styles[`mainPlayerIsOpen--${isOpen}`]
+        }`}
       >
-        {isPaused ? (
-          <PlayIcon width={12} height={12} fill="#fff" />
-        ) : (
-          <PauseIcon width={12} height={12} />
+        <Header
+          className={styles.header}
+          type="options"
+          // onBackClick={() => handleBackClick()}
+        >
+          {currentTrack.name}
+        </Header>
+        {!isOpen && (
+          <animated.div style={vanishProps} className={styles.playback}>
+            <img
+              src={
+                currentTrack.album.images[currentTrack.album.images.length - 1]
+                  .url
+              }
+              className={styles.playback__cover}
+              alt=""
+            />
+            <div className={styles.playback__info}>
+              <Typography type="h2" className={styles.info__name}>
+                {currentTrack.name}
+              </Typography>
+              <Typography type="subHeading" className={styles.info__artist}>
+                {currentTrack.artists[0].name}
+              </Typography>
+            </div>
+          </animated.div>
         )}
-      </Button>
+        <animated.div style={scaleProps} className={styles.play}>
+          {/* TODO: Insert Slider Music ProgressBar */}
+          <PlayControls
+            type={'squareBig'}
+            isPlay={!isPaused}
+            onBackwardSkip={() => player?.previousTrack()}
+            onForwardSkip={() => player?.nextTrack()}
+            togglePlay={() => player?.togglePlay()}
+            className={styles.playControls}
+          />
+        </animated.div>
+      </div>
+      <Drawer
+        className={styles.drawer}
+        display="list"
+        isOpen={isOpen}
+        onHandleClick={() => setIsOpen(!isOpen)}
+        onBackArrowClick={() => console.log('switch to listView')}
+        onOptionsClick={() => console.log('show me options')}
+      >
+        <p>Place notes Here</p>
+      </Drawer>
     </div>
   );
 }
