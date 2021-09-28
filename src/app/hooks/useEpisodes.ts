6fd8f2f24aa7a447b0259a89b9e6e4dead62d6a8
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-28 10:48:15 +0200

    ✨ Create Add Note Method

--- src/app/hooks/useEpisodes.ts
+++ src/app/hooks/useEpisodes.ts
@@ -1,10 +1,11 @@
-import type { Episode } from '../../lib/types';
+import type { Episode, Note } from '../../lib/types';
 import useLocalStorage from './useLocalStorage';
 
 export default function useEpisodes(): {
   episodeData: Episode[];
   addEpisodeData: (episode: Episode) => void;
   removeEpisodeData: (episode: Episode) => void;
+  addEpisodeNotes: (title: string, note: Note) => void;
 } {
   const [episodeData, setEpisodeData] = useLocalStorage<Episode[]>(
     'episodes',
@@ -15,9 +16,22 @@ export default function useEpisodes(): {
     setEpisodeData([...episodeData, episode]);
   }
 
+  function addEpisodeNotes(title: string, note: Note) {
+    const newData = episodeData.map((e) => {
+      if (e.title === title) {
+        e.notes?.push(note);
+        // e.notes = [...e.notes?, note ]
+        // why isnt this working?
+        console.log('inside', e.notes);
+      }
+      return e;
+    });
+    setEpisodeData(newData);
+  }
+
   function removeEpisodeData(episode: Episode) {
     setEpisodeData(episodeData.filter((b) => b !== episode));
   }
 
-  return { episodeData, addEpisodeData, removeEpisodeData };
+  return { episodeData, addEpisodeData, removeEpisodeData, addEpisodeNotes };
 }
