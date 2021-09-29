dfb607af69227f8a8f0612620888acd88e9dfdb4
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-09-29 12:29:01 +0200

    ✨ Update addEpisodes and create updateEpisodes

--- src/app/hooks/useEpisodes.ts
+++ src/app/hooks/useEpisodes.ts
@@ -5,7 +5,8 @@ export default function useEpisodes(): {
   episodeData: Episode[];
   addEpisodeData: (episode: Episode) => void;
   removeEpisodeData: (episode: Episode) => void;
-  addEpisodeNotes: (title: string, note: Note) => void;
+  addEpisodeNote: (title: string, note: Note) => void;
+  updateEpisodeNote: (title: string, note: Note) => void;
 } {
   const [episodeData, setEpisodeData] = useLocalStorage<Episode[]>(
     'episodes',
@@ -16,15 +17,31 @@ export default function useEpisodes(): {
     setEpisodeData([...episodeData, episode]);
   }
 
-  function addEpisodeNotes(title: string, note: Note) {
-    const newData = episodeData.map((e) => {
-      if (e.title === title) {
-        e.notes?.push(note);
-        // e.notes = [...e.notes?, note ]
-        // why isnt this working?
-        console.log('inside', e.notes);
+  function addEpisodeNote(id: string, newNote: Note) {
+    console.log(episodeData, 'hier');
+    const newData = episodeData.map((episode) => {
+      if (episode.id === id) {
+        episode.notes = [...episode.notes, newNote];
+        console.log('inside', episode.notes);
       }
-      return e;
+      return episode;
+    });
+    console.log(newData);
+    setEpisodeData(newData);
+  }
+
+  function updateEpisodeNote(id: string, newNote: Note) {
+    const newData = episodeData.map((episode) => {
+      if (episode.id === id) {
+        const newNotes = episode.notes.map((note) => {
+          if (note.id === newNote.id) {
+            return (note = newNote);
+          }
+          return note;
+        });
+        episode.notes = [...newNotes];
+      }
+      return episode;
     });
     setEpisodeData(newData);
   }
@@ -33,5 +50,11 @@ export default function useEpisodes(): {
     setEpisodeData(episodeData.filter((b) => b !== episode));
   }
 
-  return { episodeData, addEpisodeData, removeEpisodeData, addEpisodeNotes };
+  return {
+    episodeData,
+    addEpisodeData,
+    removeEpisodeData,
+    addEpisodeNote,
+    updateEpisodeNote,
+  };
 }
