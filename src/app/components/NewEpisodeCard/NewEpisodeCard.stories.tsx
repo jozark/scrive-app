b1bed1b5f9ee4ca45c0059c2aee4b0b6e883ac58
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-10-03 14:18:01 +0200

    🐛 Fix bug in story

--- src/app/components/NewEpisodeCard/NewEpisodeCard.stories.tsx
+++ src/app/components/NewEpisodeCard/NewEpisodeCard.stories.tsx
@@ -10,4 +10,15 @@ export default {
     },
   },
 };
-export const Import = (): JSX.Element => <NewEpisodeCard />;
+
+export const Import = (): JSX.Element => (
+  <NewEpisodeCard
+    handleButtonClick={() => console.log('button')}
+    type="import"
+    image={''}
+    title="This is the best Episode I have ever listened to"
+    show="The best Show"
+    content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti saepe ipsam, nihil illum consectetur fuga doloremque tempora non temporibus? Incidunt!"
+    time="20min"
+  />
+);
