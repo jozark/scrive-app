61461c97a8d435572ddb4f676c266d36c84395be
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-10-09 22:45:12 +0200

    ✨ Import nothing here svg

---
+++ src/app/components/assets/NothingHere.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+export default function NothingHere(): JSX.Element {
+  return (
+    <svg
+      width="269"
+      height="224"
+      viewBox="0 0 269 224"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M208.895 28.1778C234.477 41.5576 264.234 62.9652 268.462 88.6544C272.69 114.344 251.389 144.207 225.807 170.699C200.172 197.191 170.308 220.311 137.661 222.987C105.068 225.717 69.7456 208.056 42.9861 181.564C16.2265 155.072 -1.97 119.749 0.170765 86.5672C2.31153 53.3853 24.7896 22.3442 51.5491 8.96445C78.3087 -4.41533 109.35 -0.133798 135.574 4.68292C161.852 9.44612 183.26 14.798 208.895 28.1778Z"
+        fill="#1B1B1B"
+      />
+      <rect
+        x="56"
+        y="55.9426"
+        width="141"
+        height="100.483"
+        rx="10"
+        fill="url(#paint0_linear)"
+        fill-opacity="0.82"
+      />
+      <rect
+        x="66.8047"
+        y="50"
+        width="59.4253"
+        height="89.1379"
+        fill="#F2F0D7"
+      />
+      <rect x="126.23" y="50" width="59.4253" height="89.1379" fill="#EBE8D0" />
+      <rect
+        x="66.8047"
+        y="139.138"
+        width="59.4253"
+        height="8.64368"
+        fill="#EBE8D0"
+      />
+      <rect
+        x="126.23"
+        y="139.138"
+        width="59.4253"
+        height="8.64368"
+        fill="#E0DEC9"
+      />
+      <path
+        opacity="0.62"
+        d="M116.506 119.643C119.567 113.971 127.634 106.029 135.414 119.643"
+        stroke="#252525"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <path
+        opacity="0.61"
+        d="M116.506 88.8965C115.209 90.6675 113.135 92.1854 110.023 89.9085"
+        stroke="black"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <path
+        d="M141.897 89.1584C140.6 90.9294 138.526 92.4474 135.414 90.1704"
+        stroke="black"
+        stroke-opacity="0.61"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear"
+          x1="126.5"
+          y1="55.9426"
+          x2="126.5"
+          y2="156.425"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#908FDC" />
+          <stop offset="1" stop-color="#807FC0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
