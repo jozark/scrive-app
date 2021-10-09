dfa90c0c06fa1e37d96ff2889829ecbec50f5696
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-10-09 22:45:00 +0200

    ✨ Update Spotify Button to take color

--- src/app/components/SpotifyLoginButton/SpotifyLoginButton.tsx
+++ src/app/components/SpotifyLoginButton/SpotifyLoginButton.tsx
@@ -6,15 +6,20 @@ export type SpotifyLoginButtonProps = {
   children: React.ReactNode;
   className?: string;
   url: string;
+  color: 'spotify' | 'accent';
 };
 
 export default function SpotifyLoginButton({
   children,
   className,
   url,
+  color,
 }: SpotifyLoginButtonProps): JSX.Element {
   return (
-    <a href={url} className={`${styles.loginLink} ${className}`}>
+    <a
+      href={url}
+      className={`${styles.loginLink} ${className} ${styles[color]}`}
+    >
       <SpotifyLogo width={32} height={32} />
       <p className={styles.button__text}>{children}</p>
     </a>
